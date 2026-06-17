f186c84e169e31ea90cb0b2fc949ea9103e35d44
lennydefect <lenny@defect.studio>
2026-06-17 10:01:35 -0700

    cleanup

--- Plugins/Bridge/Source/MegascansPlugin/Private/Utilities/VersionInfoHandler.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-#include "VersionInfoHandler.h"
-
-#include UE_INLINE_GENERATED_CPP_BY_NAME(VersionInfoHandler)
-
-UVersionInfoHandler* UVersionInfoHandler::Get()
-{
-	return GetMutableDefault <UVersionInfoHandler>();
-};
